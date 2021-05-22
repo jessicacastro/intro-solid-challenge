7469601f9127f2d31a048956ebdb87c13b3d1325
Jessica Castro <jessica.castros@outlook.com>
2021-05-22 04:42:04 -0300

    Ready to test

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -29,19 +29,27 @@ class UsersRepository implements IUsersRepository {
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user = this.users.find((user) => user === receivedUser);
+
+    Object.assign(user, { admin: true });
+
+    return user;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,19 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+
+      const parseUserId = user_id.toString();
+
+      const listAllUsers = this.listAllUsersUseCase.execute({
+        user_id: parseUserId,
+      });
+
+      return response.status(200).json(listAllUsers);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
